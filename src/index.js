16e8e7145de697332d699d2ec1d2df4a9b77fa6d
Mark Gowen <mark.gowen78@gmail.com>
2022-11-03 10:47:29 -0700

    Lab solution, kind of

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.querySelector("#create-task-form");
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const newListItem = document.querySelector("#tasks");
+  
+  createTaskForm.addEventListener("submit", createNewTask);
 });
+
+
+const createNewTask = e => {
+  e.preventDefault();
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const newTask = document.createElement("li");
+  newTask.textContent = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  e.target.reset();
+
+}
+
+const appendNewTask = task => {
+  document.querySelector("#tasks").appendChild(task);
+}
+
